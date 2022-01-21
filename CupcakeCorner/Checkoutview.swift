da04079b64833a7b9640fb1e80e646e22ef29197
Peter Molnar <petermolnar.hu@gmail.com>
2022-01-21 19:08:49 +0100

    Day 51: Adding the final touches: Codable protocol, and URLSession.

--- CupcakeCorner/Checkoutview.swift
+++ CupcakeCorner/Checkoutview.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct Checkoutview: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
     
     var body: some View {
         ScrollView {
@@ -26,13 +28,42 @@ struct Checkoutview: View {
                     .font(.title)
                 
                 Button("Place Order") {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thanks you!", isPresented: $showingConfirmation) {
+            Button("OK") {}
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order.")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodeOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodeOrder.quantity)x \(Order.types[decodeOrder.type]) cupcakes is no the way."
+            showingConfirmation = true
+        } catch  {
+            print("Checkout failed \(error)")
+        }
     }
 }
 
