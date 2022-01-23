8fe0d10608a657877df68766459bf65a2db546c0
Peter Molnar <petermolnar.hu@gmail.com>
2022-01-23 20:58:59 +0100

    Trimming characters.

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -31,7 +31,10 @@ struct Order: Codable {
     var zip = ""
     
     var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+        if name.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+            streetAddress.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+            city.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ||
+            zip.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             return false
         }
         return true
